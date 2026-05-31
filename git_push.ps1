a6862109c7c03a9b72e2f305e889093a6eb02f05
Luiz Felipe Canedo <luizfelipe.canedo2@gmail.com>
2026-05-31 00:17:55 -0300

    feat: modernizacao de layout, graficos em degrade e alertas de pendencias (atualizacao)

--- git_push.ps1
+++ git_push.ps1
@@ -2,7 +2,10 @@ Write-Host "Iniciando Git Add..."
 git add .
 
 Write-Host "Iniciando Git Commit..."
-git commit -m "feat: modernizacao de layout, graficos em degrade e alertas de pendencias"
+git commit -m "feat: modernizacao de layout, graficos em degrade e alertas de pendencias (atualizacao)"
+
+Write-Host "Iniciando Git Pull Rebase..."
+git pull --rebase origin main
 
 Write-Host "Iniciando Git Push..."
 git push origin main
